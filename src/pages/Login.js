8c87d8afcb73fff323084087f41f01eb20f6a714
lucas damasceno <lucasd.viana1993@gmail.com>
2023-04-21 22:25:06 -0300

    adicionando login e acionando menu de opçoes para quem esta logado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import { Navigate } from 'react-router-dom';
 import "../App.css";
 import api from "../service/api";
 
+import BibliotecaContext from "../contexts/BibliotecaContext";
+
 const LoginForm = () => {
+  const [userRedirect, setUserRedirect] = useState(false)
+
+  const { setBiblioteca } = useContext(BibliotecaContext)
+
+  const setaCookie = async (value) => {
+    setBiblioteca({
+      "id":value.id,
+      "name":value.name,
+    })
+    localStorage.setItem("biblioteca", JSON.stringify(value))
+  }
 
   const enviarFormulario = async (event) => {
     event.preventDefault();
@@ -16,30 +30,36 @@ const LoginForm = () => {
       const login = await api.post('/library/login', dados)
       
       if(login.status === 200){
-        alert('Login efetuado')
+        setaCookie(login.data.data)
+        setUserRedirect(!userRedirect)
       }
-
     } catch(e){
       console.log(e)
+      alert("Email ou senha errados")
     }
   }
 
-  return (
-    <div className="formulario">
-      <div className="centralizar">
-        <center>
-          <h2>Login</h2>
-          <form onSubmit={enviarFormulario}>
-            <input type="text" placeholder="Email" id="email" required />
-            <input type="password" placeholder="Senha" id="senha" required />
-            <button className="botao1" type="submit" value="Submit">
-              Logar
-            </button>
-          </form>
-        </center>
+  if(userRedirect){
+    return (<Navigate replace to='/'/>) 
+  } else {
+    return (
+      <div className="formulario">
+        <div className="centralizar">
+          <center>
+            <h2>Login</h2>
+            <br/>
+            <form onSubmit={enviarFormulario}>
+              <input type="text" placeholder="Email" id="email" required />
+              <input type="password" placeholder="Senha" id="senha" required />
+              <button className="botao1" type="submit" value="Submit">
+                Logar
+              </button>
+            </form>
+          </center>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default LoginForm;
