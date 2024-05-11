933a0f25d13379bdd78e12bf54f7685c79d2b78e
lucasd <lucasd.viana1993@gmail.com>
2024-05-11 19:41:31 -0300

    fix data

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,10 +2,16 @@ import React from "react";
 import Container from "./Container";
 
 const Footer = () => {
+
+    function getAnoAtual() {
+        const dataAtual = new Date();
+        return dataAtual.getFullYear();
+      }
+
     return(
         <Container>
             <footer>
-                <p>&copy;Sala de Leitura-Univesp 2023</p>
+                <p>&copy;Sala de Leitura-Univesp {getAnoAtual()}</p>
             </footer>
         </Container>
     )
